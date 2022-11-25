6eb7023b746341f3e2df16b06e4a0d87644e9c2c
DeeCaaD <perttu.kangas@hotmail.fi>
2022-11-25 16:05:57 +0200

    5.3

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -13,7 +13,7 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        return sum(o.hinta() for o in list(self.ostokset.values()))
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
