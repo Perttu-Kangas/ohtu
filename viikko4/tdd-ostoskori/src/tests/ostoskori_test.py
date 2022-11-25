98fd2f24effb46eb7911433e55868b36d4079cf8
DeeCaaD <perttu.kangas@hotmail.fi>
2022-11-25 16:28:04 +0200

    5.10

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -41,11 +41,15 @@ class TestOstoskori(unittest.TestCase):
     
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
         self.kori.lisaa_tuote(self.def_tuote[0])
-        ostokset = self.kori.ostokset()
-        self.assertEqual(len(ostokset), 1)
+        self.assertEqual(len(self.kori.ostokset()), 1)
     
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
         self.kori.lisaa_tuote(self.def_tuote[0])
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.lukumaara(), 1)
         self.assertEqual(ostos.tuotteen_nimi(), "yeet")
+    
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosolio(self):
+        self.kori.lisaa_tuote(self.def_tuote[0])
+        self.kori.lisaa_tuote(self.def_tuote[1])
+        self.assertEqual(len(self.kori.ostokset()), 2)
