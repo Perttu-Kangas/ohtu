739d33f148b3840c0a70aee8d11cf33587b5f243
DeeCaaD <perttu.kangas@hotmail.fi>
2022-12-04 17:59:45 +0200

    5.4-5.5

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,15 +1,54 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
+        self.edellinen = None
 
     def miinus(self, arvo):
-        self.tulos = self.tulos - arvo
+        self.aseta_arvo(self.tulos - arvo)
 
     def plus(self, arvo):
-        self.tulos = self.tulos + arvo
+        self.aseta_arvo(self.tulos + arvo)
 
     def nollaa(self):
-        self.tulos = 0
+        self.aseta_arvo(0)
 
     def aseta_arvo(self, arvo):
+        self.edellinen = self.tulos
         self.tulos = arvo
+    
+    def kumoa(self):
+        if self.edellinen:
+            self.tulos = self.edellinen
+        self.edellinen = None
+
+class Summa:
+    def __init__(self, sovellus, syote):
+        self.sovellus = sovellus
+        self.syote = syote
+
+    def suorita(self):
+        return self.sovellus.plus(int(self.syote()))
+
+class Erotus:
+    def __init__(self, sovellus, syote):
+        self.sovellus = sovellus
+        self.syote = syote
+
+    def suorita(self):
+        return self.sovellus.miinus(int(self.syote()))
+
+class Nollaus:
+    def __init__(self, sovellus, syote):
+        self.sovellus = sovellus
+        self.syote = syote
+
+    def suorita(self):
+        return self.sovellus.nollaa()
+
+class Kumoa:
+    def __init__(self, sovellus, syote):
+        self.sovellus = sovellus
+        self.syote = syote
+
+    def suorita(self):
+        return self.sovellus.kumoa()
