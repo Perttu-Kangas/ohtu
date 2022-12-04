660cc99e321cee94057bc903d73be3020dcb0a0b
DeeCaaD <perttu.kangas@hotmail.fi>
2022-12-04 17:39:47 +0200

    5.3

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,57 +1,35 @@
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1 = player1_name
+        self.player2 = player2_name
+        self.score1 = 0
+        self.score2 = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1:
+            self.score1 += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.score2 += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if self.score1 == self.score2:
+            return self._get_by_score(self.score1, all=True)
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+        if max(self.score1, self.score2) < 4:
+            return f"{self._get_by_score(self.score1)}-{self._get_by_score(self.score2)}"
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+        return self._get_by_diff()
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+    
+    def _get_by_score(self, id, all=None):
+        array = ["Love", "Fifteen", "Thirty", "Forty"]
+        if id > 3:
+            return "Deuce"
+        return array[id] if all is None else f"{array[id]}-All"
+    
+    def _get_by_diff(self):
+        score_diff = self.score1 - self.score2
+        ahead_player = self.player1 if score_diff > 0 else self.player2
+        if abs(score_diff) == 1:
+            return f"Advantage {ahead_player}"
+        return f"Win for {ahead_player}"
